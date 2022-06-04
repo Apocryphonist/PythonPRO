6842ba65a96990c5968bf37cdcc42127cef2333a
Hasan <Apocryphonist@users.noreply.github.com>
2022-06-04 14:11:46 +0000

    Initial commit

---
+++ Day 1/Ex2.py
@@ -0,0 +1,13 @@
+#Write your code below this line
+
+
+print(len(input("What is your name? ")))
+
+#input will get the name 
+#len will find out the length
+#print will print the lenght of the name 
+
+
+
+
+
---
+++ Day 1/Ex3.py
@@ -0,0 +1,4 @@
+#Write your code below this line
+
+String = input("What is your name?")
+print(len(String))
